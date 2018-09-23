986a04957a46761d72d02a76581167fdb1a89ea6
ematsan <matsan_e_m@yahoo.com>
2018-09-23 12:58:15 +0300

    Finall

--- script.js
+++ script.js
@@ -27,11 +27,14 @@ var MyCube = Object.create(HTMLElement.prototype);
         {
           let newButton = document.createElement('button');
           newButton.className = "bt";
+          newButton.className = "bt b" + i + j;
           newButton.style.width = this.cube + "px";
           newButton.style.height = this.cube + "px";
+          newButton.style.top = this.cube * i + "px";
+          newButton.style.left = this.cube * j + "px";
           maindiv.appendChild(newButton);
         }
-        var newButton = document.createElement('button');
+        let newButton = document.createElement('button');
         newButton.className = "bt right";
         newButton.innerHTML = '+';
         newButton.onclick = this.mClickR.bind(this);
@@ -59,31 +62,35 @@ var MyCube = Object.create(HTMLElement.prototype);
         newButton.style.width = this.cube + "px";
         newButton.style.height = this.cube + "px";
         maindiv.appendChild(newButton);
-      var body = document.querySelector("body");
+      let body = document.querySelector("body");
       body.appendChild(workdiv);
    }
 
 MyCube.mOwer = function () {
-  var left = document.querySelector(".left");
-  var top = document.querySelector(".top");
-  var work = document.querySelector(".work");
-  var x = event.pageX==undefined?event.pageX:event.clientX;
-  var y = event.pageY==undefined?event.pageX:event.clientY;
-  if (this.cols > 1)
-    top.style.visibility = "visible";
-  if (this.rows > 1)
-    left.style.visibility = "visible";
+  let left = document.querySelector(".left");
+  let top = document.querySelector(".top");
+  let work = document.querySelector(".work");
+  let x = event.pageX==undefined?event.pageX:event.clientX;
+  let y = event.pageY==undefined?event.pageX:event.clientY;
   let str = work.style.left;
   str = str.substring(0, str.length - 2);
   x = x - Number(str) - this.cube;
   str = work.style.top;
   str = str.substring(0, str.length - 2);
   y = y - Number(str) - this.cube;
+  if ((top.style.visibility == "hidden") && (y < 0))
+    return;
+  if ((left.style.visibility == "hidden") && (x < 0))
+      return;
+  if (this.cols > 1)
+    top.style.visibility = "visible";
+  if (this.rows > 1)
+    left.style.visibility = "visible";
   if (y > 2)
-    left.style.top = Math.floor(y / this.cube) * (this.cube)+ "px";
+    left.style.top = Math.floor(y / this.cube) * this.cube + "px";
   if (x > 2)
-    top.style.left = Math.floor(x / this.cube) * this.cube+ "px";
-  if (x > (this.cols * this.cube) || y > (this.rows * this.cube) || ((x < 0) && (y < 0)))
+    top.style.left = Math.floor(x / this.cube) * this.cube + "px";
+  if (x > (this.cols * this.cube - 2) || y > (this.rows * this.cube - 2) || ((x < 0) && (y < 0)))
   {
     left.style.visibility = "hidden";
     top.style.visibility = "hidden";
@@ -91,62 +98,71 @@ MyCube.mOwer = function () {
 }
 
 MyCube.mClickR = function () {
-  this.cols++;
-  var work = document.querySelector(".work");
-  var main = document.querySelector(".main");
-  work.style.width = (this.cols + 2) * this.cube + "px";
-  main.style.width = this.cols * this.cube+ "px";
-  for (let i = 0; i<this.rows; i++){
-     var newButton = document.createElement('button');
-     newButton.className = "bt";
+  let work = document.querySelector(".work");
+  let main = document.querySelector(".main");
+  for (let i = this.rows - 1; i >= 0; i--){
+     let newButton = document.createElement('button');
+     newButton.className = "bt b" + i + this.cols;
      newButton.style.width = this.cube + "px";
      newButton.style.height = this.cube + "px";
+     newButton.style.top = this.cube * i + "px";
+     newButton.style.left = this.cube * this.cols+ "px";
      main.insertBefore(newButton,main.children[0]);
    }
+   this.cols++;
+   work.style.width = (this.cols + 2) * this.cube+ "px";
+   main.style.width = this.cols * this.cube+ "px";
 }
 
 MyCube.mClickT = function() {
+  let left = document.querySelector(".left");
+  let top = document.querySelector(".top");
+  let work = document.querySelector(".work");
+  let main = document.querySelector(".main");
   this.cols--;
-  var left = document.querySelector(".left");
-  var top = document.querySelector(".top");
   left.style.visibility = "hidden";
   top.style.visibility = "hidden";
-  var work = document.querySelector(".work");
-  var main = document.querySelector(".main");
+  for (let i = this.rows - 1; i >=  0; i--){
+     let str = ".b" + i + this.cols;
+     let btn = main.querySelector(str);
+     btn.remove();
+   }
   work.style.width = (this.cols + 2)* this.cube+ "px";
   main.style.width = this.cols * this.cube+ "px";
-  for (let i = 0; i < this.rows; i++)
-     main.removeChild(main.children[i]);
 }
 
 MyCube.mClickB = function () {
-  this.rows++;
-  var work = document.querySelector(".work");
-  var main = document.querySelector(".main");
-  work.style.height = (this.rows + 2) * this.cube+ "px";
-  main.style.height = this.rows * this.cube+ "px";
+  let work = document.querySelector(".work");
+  let main = document.querySelector(".main");
   for (let i = 0; i < this.cols; i++){
-     var newButton = document.createElement('button');
-     newButton.className = "bt";
+     let newButton = document.createElement('button');
+     newButton.className = "bt b" + this.rows + i;
      newButton.style.width = this.cube + "px";
      newButton.style.height = this.cube + "px";
-     main.insertBefore(newButton,main.children[0]);
+     newButton.style.top = this.cube * this.rows + "px";
+     newButton.style.left = this.cube * i + "px";
+     main.appendChild(newButton);
    }
+   this.rows++;
+   work.style.height = (this.rows + 2) * this.cube+ "px";
+   main.style.height = this.rows * this.cube+ "px";
 }
 
 MyCube.mClickL = function (){
+  let left = document.querySelector(".left");
+  let top = document.querySelector(".top");
+  let work = document.querySelector(".work");
+  let main = document.querySelector(".main");
   this.rows--;
-  var left = document.querySelector(".left");
-  var top = document.querySelector(".top");
   left.style.visibility = "hidden";
   top.style.visibility = "hidden";
-  var work = document.querySelector(".work");
-  var main = document.querySelector(".main");
+  for (let i = 0; i < this.cols; i++){
+     let str = ".b" + this.rows + i;
+     let btn = main.querySelector(str);
+     btn.remove();
+   }
   work.style.height = (this.rows + 2)* this.cube+ "px";
   main.style.height = this.rows * this.cube+ "px";
-  for (let i = 0; i < this.cols; i++){
-     main.removeChild(main.children[i]);
-   }
 }
 
 // регистрируем новый элемент в браузере
