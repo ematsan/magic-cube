090586a57de910952f3a4c0873972ce214dd0cae
ematsan <matsan_e_m@yahoo.com>
2018-09-26 14:23:16 +0300

    remarks were corrected

--- class/cube.js
+++ class/cube.js
@@ -1,158 +1,187 @@
 class MyCube {
+  //конструктор
   constructor(rows, cols, cube){
     this.rows = rows;
     this.cols = cols;
     this.cube = cube;
-    let workdiv = document.createElement('div');
-    workdiv.className = "work";
-    workdiv.style.width = this.cube * (this.cols + 2) + "px";
-    workdiv.style.height = this.cube * (this.rows + 2) + "px";
-    workdiv.style.left = this.cube + "px"
-    workdiv.style.top = this.cube + "px"
-    workdiv.onmousemove = this.mOwer.bind(this);
-    let maindiv = document.createElement('div');
-    maindiv.className = "main";
-    maindiv.style.width = this.cube * this.cols + "px";
-    maindiv.style.height = this.cube * this.rows + "px";
-    maindiv.style.left = this.cube + "px"
-    maindiv.style.top = this.cube + "px"
-    workdiv.appendChild(maindiv);
+
+    this.border = 2;
+
+    let wraperdiv = this.creatDiv("wraper", this.cube * (this.cols + 2), this.cube * (this.rows + 2));
+    wraperdiv.onmousemove = this.mouseOwer.bind(this);
+
+    let maindiv = this.creatDiv("main", this.cube * this.cols + this.border, this.cube * this.rows + this.border);
+    wraperdiv.appendChild(maindiv);
+
     for (let i = 0; i < this.rows; i++)
       for (let j = 0; j < this.cols; j++)
-      {
-        let newButton = document.createElement('button');
-        newButton.className = "bt";
-        newButton.className = "bt b" + i + j;
-        newButton.style.width = this.cube + "px";
-        newButton.style.height = this.cube + "px";
-        newButton.style.top = this.cube * i + "px";
-        newButton.style.left = this.cube * j + "px";
-        maindiv.appendChild(newButton);
-      }
-      let newButton = document.createElement('button');
-      newButton.className = "bt right";
-      newButton.innerHTML = '+';
-      newButton.onclick = this.mClickR.bind(this);
-      newButton.style.width = this.cube + "px";
-      newButton.style.height = this.cube + "px";
-      maindiv.appendChild(newButton);
-      newButton = document.createElement('button');
-      newButton.className = "bt bottom";
-      newButton.innerHTML = '+';
-      newButton.onclick = this.mClickB.bind(this);
-      newButton.style.width = this.cube + "px";
-      newButton.style.height = this.cube + "px";
-      maindiv.appendChild(newButton);
-      newButton = document.createElement('button');
-      newButton.className = "bt top";
-      newButton.innerHTML = '-';
-      newButton.onclick = this.mClickT.bind(this);
-      newButton.style.width = this.cube + "px";
-      newButton.style.height = this.cube + "px";
-      maindiv.appendChild(newButton);
-      newButton = document.createElement('button');
-      newButton.className = "bt left";
-      newButton.innerHTML = '-';
-      newButton.onclick = this.mClickL.bind(this);
-      newButton.style.width = this.cube + "px";
-      newButton.style.height = this.cube + "px";
-      maindiv.appendChild(newButton);
+        maindiv.appendChild(this.creatElement(i,j));
+
+    let button = this.creatButton("right", "+");
+    button.onclick = this.mouseClickRight.bind(this);
+    maindiv.appendChild(button);
+
+    button = this.creatButton("bottom", "+");
+    button.onclick = this.mouseClickBottom.bind(this);
+    maindiv.appendChild(button);
+
+    button = this.creatButton("top", "-");
+    button.onclick = this.mouseClickTop.bind(this);
+    maindiv.appendChild(button);
+
+    button = this.creatButton("left", "-");
+    button.onclick = this.mouseClickLeft.bind(this);
+    maindiv.appendChild(button);
+
     let body = document.querySelector("body");
-    body.appendChild(workdiv);
+    body.appendChild(wraperdiv);
  }
 
-   mOwer() {
-      let left = document.querySelector(".left");
-      let top = document.querySelector(".top");
-      let work = document.querySelector(".work");
-      let x = event.pageX==undefined?event.pageX:event.clientX;
-      let y = event.pageY==undefined?event.pageX:event.clientY;
-      x = x - this.cube - work.getBoundingClientRect().left;
-      y = y - this.cube - work.getBoundingClientRect().top;
-      if ((top.style.visibility == "hidden") && (y < 0))
-        return;
-      if ((left.style.visibility == "hidden") && (x < 0))
-        return;
-      if (this.cols > 1)
-        top.style.visibility = "visible";
-      if (this.rows > 1)
-        left.style.visibility = "visible";
-      if (y > 2)
-        left.style.top = Math.floor(y / this.cube) * this.cube + "px";
-      if (x > 2)
-        top.style.left = Math.floor(x / this.cube) * this.cube + "px";
-      if (x > (this.cols * this.cube - 2) || y > (this.rows * this.cube - 2) || ((x < 0) && (y < 0)))
-      {
-        left.style.visibility = "hidden";
-        top.style.visibility = "hidden";
-      }
+   //создание рабочей области
+   creatDiv(str, wid, heig){
+     let newDiv = document.createElement('div');
+     newDiv.className = str;
+     newDiv.style.width = wid + "px";
+     newDiv.style.height = heig + "px";
+     newDiv.style.top = this.cube + "px";
+     newDiv.style.left = this.cube + "px";
+     return newDiv;
+   }
+
+   //создание элементов в заданной позиции
+   creatElement(col, row) {
+     let newElement = document.createElement('div');
+     newElement.className = "btn b" + col + row;
+     newElement.style.width = this.cube + "px";
+     newElement.style.height = this.cube + "px";
+     newElement.style.top = this.cube * col + "px";
+     newElement.style.left = this.cube * row + "px";
+     return newElement;
+   }
+
+   //создание кнопок с заданным текстовм
+   creatButton(str, val){
+     let newButton = document.createElement('button');
+     newButton.className = "btn "+ str;
+     newButton.innerHTML = val;
+     newButton.style.width = this.cube + "px";
+     newButton.style.height = this.cube + "px";
+     return newButton;
+   }
+
+   //изменение рабочей области
+   changeDiv(){
+     let wraper = document.querySelector(".wraper");
+     let main = document.querySelector(".main");
+     wraper.style.height = (this.rows + 2) * this.cube+ "px";
+     main.style.height = this.rows * this.cube + this.border + "px";
+     wraper.style.width = (this.cols + 2) * this.cube+ "px";
+     main.style.width = this.cols * this.cube + this.border + "px";
+   }
+
+   //скрытие кнопок
+   hidenButton(){
+     let left = document.querySelector(".left");
+     let top = document.querySelector(".top");
+     left.style.visibility = "hidden";
+     top.style.visibility = "hidden";
+   }
+
+   //отображение кнопок
+   showButton(x, y){
+     let left = document.querySelector(".left");
+     let top = document.querySelector(".top");
+     if (this.cols > 1)
+       top.style.visibility = "visible";
+     if (this.rows > 1)
+       left.style.visibility = "visible";
+     if (event.target.className !== "main")
+       {
+       left.style.top = y;
+       top.style.left = x;
+     }
+   }
+
+   //преобразование строки размера в число
+   convertPx(str){
+     let rez = str;
+     rez = rez.substring(0, rez.length - 2);
+     return rez;
+   }
+
+   //движение мыши
+   mouseOwer(event) {
+     if  (event.target.className.indexOf("wraper") != -1 ||
+          event.target.className.indexOf("bottom") !=-1 ||
+          event.target.className.indexOf("right") !=-1)
+        this.hidenButton();
+     else
+        this.showButton(event.target.style.left, event.target.style.top)
       }
 
-    mClickR() {
-      let work = document.querySelector(".work");
+    //клик на правой кнопке
+    mouseClickRight(event) {
       let main = document.querySelector(".main");
-      for (let i = this.rows - 1; i >= 0; i--){
-         let newButton = document.createElement('button');
-         newButton.className = "bt b" + i + this.cols;
-         newButton.style.width = this.cube + "px";
-         newButton.style.height = this.cube + "px";
-         newButton.style.top = this.cube * i + "px";
-         newButton.style.left = this.cube * this.cols+ "px";
-         main.insertBefore(newButton,main.children[0]);
+      for (let i = 0; i < this.rows; i++){
+         main.appendChild(this.creatElement(i, this.cols));
        }
        this.cols++;
-       work.style.width = (this.cols + 2) * this.cube+ "px";
-       main.style.width = this.cols * this.cube+ "px";
+       this.changeDiv();
       }
 
-    mClickT() {
-      let left = document.querySelector(".left");
-      let top = document.querySelector(".top");
-      let work = document.querySelector(".work");
+    //клик на верхней кнопке
+    mouseClickTop(event) {
+      this.hidenButton();
       let main = document.querySelector(".main");
-      this.cols--;
-      left.style.visibility = "hidden";
-      top.style.visibility = "hidden";
-      for (let i = this.rows - 1; i >=  0; i--){
-         let str = ".b" + i + this.cols;
+      let n = this.convertPx(event.target.style.left)/ this.cube;
+      for (let i = 0; i < this.rows; i++){
+         let str = ".b" + i + n;
          let btn = main.querySelector(str);
          btn.remove();
+         if (n < this.cols){
+           for (let j = n + 1; j<this.cols; j++)
+            {
+              let old = ".b" + i + j;
+              btn = main.querySelector(old);
+              btn.className = "btn b" + i + (j - 1);
+              btn.style.left = this.convertPx(btn.style.left) - this.cube + "px";
+            }
+          }
        }
-      work.style.width = (this.cols + 2)* this.cube+ "px";
-      main.style.width = this.cols * this.cube+ "px";
+       this.cols--;
+       this.changeDiv();
       }
 
-   mClickB() {
-      let work = document.querySelector(".work");
+   //клик на нижней кнопке
+   mouseClickBottom(event) {
       let main = document.querySelector(".main");
       for (let i = 0; i < this.cols; i++){
-         let newButton = document.createElement('button');
-         newButton.className = "bt b" + this.rows + i;
-         newButton.style.width = this.cube + "px";
-         newButton.style.height = this.cube + "px";
-         newButton.style.top = this.cube * this.rows + "px";
-         newButton.style.left = this.cube * i + "px";
-         main.appendChild(newButton);
+         main.appendChild(this.creatElement(this.rows, i));
        }
        this.rows++;
-       work.style.height = (this.rows + 2) * this.cube+ "px";
-       main.style.height = this.rows * this.cube+ "px";
+       this.changeDiv();
       }
 
-    mClickL(){
-      let left = document.querySelector(".left");
-      let top = document.querySelector(".top");
-      let work = document.querySelector(".work");
+    //клик на левой кнопке
+    mouseClickLeft(event){
+      this.hidenButton();
       let main = document.querySelector(".main");
-      this.rows--;
-      left.style.visibility = "hidden";
-      top.style.visibility = "hidden";
+      let n = this.convertPx(event.target.style.top)/ this.cube;
       for (let i = 0; i < this.cols; i++){
-         let str = ".b" + this.rows + i;
+         let str = ".b" + n + i;
          let btn = main.querySelector(str);
          btn.remove();
+         if (n < this.rows){
+           for (let j = n + 1; j<this.rows; j++)
+            {
+              let old = ".b" + j + i;
+              btn = main.querySelector(old);
+              btn.className = "btn b" + (j - 1) + i;
+              btn.style.top = this.convertPx(btn.style.top) - this.cube + "px";
+            }
+          }
        }
-      work.style.height = (this.rows + 2)* this.cube+ "px";
-      main.style.height = this.rows * this.cube+ "px";
+       this.rows--;
+       this.changeDiv();
       }
 }
